dd53c58bbdf0081ac509e7c4a0d9649594595bf9
Adam Cunnington <adamcunnington.info@gmail.com>
2014-06-04 18:28:18 +0100

    Setup updated to use setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name="OlympianAdmin",
+      version="0.1",
+      description="Administration System for Counter-Strike: Source Servers",
+      author="Adam Cunnington",
+      author_email="ac@adamcunnington.info",
+      license="GPLv3",
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Other Environment",
+          "Intended Audience :: Other Audience",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.5",
+          "Topic :: Games/Entertainment :: First Person Shooters"],
+      keywords=("css counter-strike 'counter-strike: source', server "
+                "administration"),
+      packages=find_packages(exclude=(".git", )))
